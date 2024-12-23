33b99f2c6623fa6e3d15d29befd58d4f40592cf0
Thailand2550-Chin <yoyofive4@gmail.com>
2024-12-23 14:33:11 +0700

    Add files via upload

---
+++ งาน003.py
@@ -0,0 +1,10 @@
+X = float(input("กว้าง:"))
+Y = float(input("ยาว:"))
+Z = float(input("สูง:"))
+MN = X * Y * Z  
+FN = MN * 15
+BP = round(FN,2)
+MO = BP / 60
+CN = round(MO,2)
+print(CN, "นาที")
+
